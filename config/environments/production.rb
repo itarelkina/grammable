4c7226392342a8d9c2450651b358dede0d649028
Irina Tarelkina <itarelkina@yahoo.ca>
2019-04-13 19:37:42 +0000

    Lesson 18 to fix heroku issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'grammable-kresmlin-palmore.herokuapp.com'}
-  # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.default_url_options = { host: 'grammable-irina-tarelkina.herokuapp.com' }
+    # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
   config.cache_classes = true
